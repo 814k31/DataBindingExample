14caad4372f159e18540921cc97f42a5db1334de
Blake Lovelace <blake.lovelace@health.telstra.com>
2022-07-17 19:56:38 +0800

    Improve example

--- DataBindingExample/Card.xaml.cs
+++ DataBindingExample/Card.xaml.cs
@@ -2,7 +2,14 @@ namespace DataBindingExample;
 
 public partial class Card : VerticalStackLayout
 {
+    public static readonly BindableProperty CardTitleProperty = BindableProperty.Create(nameof(CardTitle), typeof(string), typeof(Card), string.Empty);
     public static readonly BindableProperty CardIncrementProperty = BindableProperty.Create(nameof(CardIncrement), typeof(int), typeof(Card), 0);
+
+    public string CardTitle
+    {
+        get => (string)GetValue(CardTitleProperty);
+        set => SetValue(CardTitleProperty, value);
+    }
 
     public int CardIncrement
     {
